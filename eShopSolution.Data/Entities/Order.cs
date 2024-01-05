fc1fe70f01594b5723ba667b86f9add8d6c9066d
dotrangtuan <dotrangtuan02@gmail.com>
2024-01-05 09:59:59 +0700

    add ProjectWebApp

---
+++ eShopSolution.Data/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using eShopSolution.Data.Enums;
+
+namespace eShopSolution.Data.Entities;
+
+public class Order
+{
+    public Guid Id { set; get; }
+    public DateTime OrderDate { set; get; }
+    public Guid UserId { set; get; }
+    public string ShipName { set; get; }
+    public string ShipAddress { set; get; }
+    public string ShipEmail { set; get; }
+    public string ShipPhoneNumber { set; get; }
+    public OrderStatus Status { set; get; }
+
+    public List<OrderDetail> OrderDetails { get; set; }
+}
